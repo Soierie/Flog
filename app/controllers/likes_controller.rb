44eccd35ae349fe42875103b71e54760faa30626
Soierie <mirror.goat1999@gmail.com>
2022-05-29 12:44:10 +0000

    production edit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+
   def create
     flog = WrritenFlog.find(params[:flog_id])
     like = current_user.likes.new(user_id: current_user.id, wrriten_flog_id: params[:flog_id])
@@ -9,6 +10,5 @@ class LikesController < ApplicationController
     flog = WrritenFlog.find(params[:flog_id])
     like = current_user.likes.find_by(user_id: current_user.id, wrriten_flog_id: params[:flog_id])
     like.destroy
-    redirect_to flog_path(flog.movie_id)
   end
 end
